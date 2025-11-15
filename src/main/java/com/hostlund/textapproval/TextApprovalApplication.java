d201f424ac50a86a0954ec4eefc01de8e4c636a0
Oskar Höstlund <oskarhostlund@gmail.com>
2025-11-15 14:58:46 +0100

    init commit

---
+++ src/main/java/com/hostlund/textapproval/TextApprovalApplication.java
@@ -0,0 +1,13 @@
+package com.hostlund.textapproval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextApprovalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextApprovalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hostlund/textapproval/TextApprovalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostlund.textapproval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextApprovalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
